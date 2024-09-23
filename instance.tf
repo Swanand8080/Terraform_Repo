35efad89a39791faf41cd4d8053d46b36c6463ab
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2024-09-23 16:37:49 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,24 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  
+  tags = {
+    Name = "allow_tls"
+  }
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = ["0.0.0.0/0"]
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+}
 resource "aws_instance" "web1" {
   ami           = "ami-0583d8c7a9c35822c"
   instance_type = "t2.micro"
-  lifecycle {
-    create_before_destroy = true
-  }
-  tags = {
+  security_group = [aws_security_group.allow_tls.id]
+  
+    tags = {
     Name      = "TF Cloud Instance"
   }
 }
