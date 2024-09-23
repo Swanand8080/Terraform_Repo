98d2ec455b1afeae8e2a66049b56626d5c2f2cd8
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2024-09-23 16:14:51 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web1" {
   ami           = "ami-0583d8c7a9c35822c"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   tags = {
     Name      = "TF Cloud Instance"
   }
