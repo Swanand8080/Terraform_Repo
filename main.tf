3418e3ca8aa15b81650bb7822c648cfc3ac3b5d2
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2024-09-23 15:42:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.67.0"
+    }
+  }
+}
+
+provider "aws" {
+        region     = "us-east-1"
+}
+resource "aws_ebs_volume" "myvolume" {
+  availability_zone = "us-east-1a"
+  size              = 40
+
+  tags = {
+    Name = "CreatedUsingTerraform"
+  }
+}
