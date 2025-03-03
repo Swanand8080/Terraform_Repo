c77c12b9693077f7f9b73bde8aeffc489286b54d
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2025-03-03 17:28:26 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "vpc-web" {
 
 resource "aws_instance" "web" {
   ami           = "ami-05b10e08d247fb927"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   availability_zone = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.vpc-web.id]
 }
